d43ef9d7d07075fbf449b027229c7d87d2fac26b
Akshaydev89036 <Akshaydev89036@gmail.com>
2023-07-04 23:26:47 -0400

    Readme file edited

--- bashscripts/package.sh
+++ bashscripts/package.sh
@@ -3,7 +3,15 @@
 # variable declaring for the package
 PACKAGE="apache2"
 
+sudo systemctl status $PACKAGE | grep 'running'
 
+if [ $? -eq 0 ]
+
+
+then
+	echo "the package is already insatlled"
+
+else
 
 echo "Installing package."
 sudo apt install $PACKAGE -y
@@ -14,10 +22,9 @@ sudo systemctl start $PACKAGE
 
 echo "Status of the service"
 sudo systemctl status $PACKAGE
+ 
 
 
 
 
-
-
-
+fi
